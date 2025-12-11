6a6bf207f3365f31e7649d82e03e141ea7bdabbd
rasyadegan17-glitch <rasyad.egan17@gmail.com>
2025-12-11 21:43:34 +0700

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-# ==============================================================================
-# BAGIAN 1: CLASS PLANNER (MODEL)
-# ==============================================================================
 class Planner:
     # PERHATIKAN: Ada 2 garis bawah sebelum 'init' dan 2 sesudahnya.
     # def _init_ (bukan init)
@@ -13,8 +10,6 @@ class Planner:
         self._tanggal = tanggal
         self.aktivitas = aktivitas
 
-    # --- Getters & Setters ---
-    @property
     def tujuan(self):
         return self._tujuan
 
@@ -203,6 +198,7 @@ class App:
 if __name__ == "__main__":
     app = App()
     app.run()
+
 
 
 
